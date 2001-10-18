2b192066c165f44ff23cfe30e1776f79c6eb5cd0
kohler <kohler>
2001-10-18 07:16:00 +0000

    add AggregateLength; little updates

---
+++ models/package/collectflows.cc
@@ -0,0 +1,303 @@
+// -*- mode: c++; c-basic-offset: 4 -*-
+#include <config.h>
+#include <click/config.h>
+
+#include "collectflows.hh"
+#include <click/error.hh>
+#include <click/hashmap.hh>
+#include <click/straccum.hh>
+#include <click/confparse.hh>
+#include <click/click_ip.h>
+#include <click/click_tcp.h>
+#include <click/packet_anno.hh>
+
+// flow
+
+bool
+CollectTCPFlows::Flow::make_pair(const IPFlowID &flow, Flow **flow1, Flow **flow2)
+{
+    *flow1 = new Flow(flow, true);
+    *flow2 = new Flow(flow.rev(), false);
+    if (*flow1 && *flow2) {
+	(*flow1)->_reverse = *flow2;
+	(*flow2)->_reverse = *flow1;
+	return true;
+    } else {
+	delete *flow1;
+	delete *flow2;
+	*flow1 = *flow2 = 0;
+	return false;
+    }
+}
+
+inline void
+CollectTCPFlows::Flow::update(const Packet *p, CollectTCPFlows *cf)
+{
+    // check for TCP flags
+    const click_tcp *tcph = p->tcp_header();
+    if (IP_FIRSTFRAG(p->ip_header())) {
+	if (tcph->th_flags & TH_RST)
+	    set_session_over();
+	else if (tcph->th_flags & TH_FIN)
+	    set_flow_over();
+	else if ((tcph->th_flags & TH_SYN) && session_over()) {
+	    // write out flow, clear state
+	    cf->write_flow(this);
+	    clear(true);
+	    reverse()->clear(false);
+	}
+    }
+    
+    _packet_count++;
+    _byte_count += p->length() + EXTRA_LENGTH_ANNO(p);
+    _last_ts = p->timestamp_anno();
+    if (!_first_ts.tv_sec)	// XXX
+	_first_ts = _last_ts;
+}
+
+// element
+
+CollectTCPFlows::CollectTCPFlows()
+    : Element(1, 1), _map(0), _f(0)
+{
+    MOD_INC_USE_COUNT;
+}
+
+CollectTCPFlows::~CollectTCPFlows()
+{
+    MOD_DEC_USE_COUNT;
+    uninitialize();
+}
+
+void
+CollectTCPFlows::notify_noutputs(int n)
+{
+    set_noutputs(n <= 1 ? 1 : 2);
+}
+
+int
+CollectTCPFlows::configure(const Vector<String> &conf, ErrorHandler *errh)
+{
+    if (cp_va_parse(conf, this, errh,
+		    cpFilename, "dump filename", &_filename,
+		    0) < 0)
+	return -1;
+    return 0;
+}
+
+int
+CollectTCPFlows::initialize(ErrorHandler *errh)
+{
+    assert(!_f);
+    if (_filename != "-") {
+	_f = fopen(_filename, "wb");
+	if (!_f)
+	    return errh->error("%s: %s", _filename.cc(), strerror(errno));
+    } else {
+	_f = stdout;
+	_filename = "<stdout>";
+    }
+    
+    _last_flow = _done_head = _done_tail = 0;
+    timerclear(&_done_timestamp);
+    return 0;
+}
+
+void
+CollectTCPFlows::uninitialize()
+{
+    if (_f && _f != stdout)
+	fclose(_f);
+    _f = 0;
+    clear(false);
+}
+
+void
+CollectTCPFlows::clear(bool write_flows)
+{
+    Vector<Flow *>poo;
+    Flow *to_free = 0;
+
+    for (Map::Iterator iter = _map.first(); iter; iter++)
+	if (Flow *m = iter.value()) {
+	    if (m->is_primary()) {
+		m->_free_next = to_free;
+		to_free = m;
+		poo.push_back(m);
+	    }
+	}
+
+    while (to_free) {
+	assert(poo.back() == to_free);
+	poo.pop_back();
+	Flow *n = to_free->_free_next;
+	if (write_flows)
+	    write_flow(to_free);
+	delete to_free->reverse();
+	delete to_free;
+	to_free = n;
+    }
+
+    _map.clear();
+    _last_flow = _done_head = _done_tail = 0;
+}
+
+Packet *
+CollectTCPFlows::bad_packet(Packet *p)
+{
+    if (noutputs() == 2)
+	output(1).push(p);
+    else
+	p->kill();
+    return 0;
+}
+
+CollectTCPFlows::Flow *
+CollectTCPFlows::add_flow(const IPFlowID &flowid, Packet *)
+{
+    Flow *flow1, *flow2;
+    if (Flow::make_pair(flowid, &flow1, &flow2)) {
+	_map.insert(flow1->flow_id(), flow1);
+	_map.insert(flow2->flow_id(), flow2);
+    } else
+	click_chatter("%s: out of memory!", declaration().cc());
+    return flow1;
+}
+
+inline CollectTCPFlows::Flow *
+CollectTCPFlows::Flow::free_from_free(Map &map)
+{
+    // see also clear_map below
+    assert(is_primary());
+    //click_chatter("kill %s", reverse()->flow_id().rev().s().cc());
+    Flow *next = _free_next;
+    map.remove(flow_id());
+    map.remove(reverse()->flow_id());
+    delete reverse();
+    delete this;
+    return next;
+}
+
+void
+CollectTCPFlows::pass_over_done(const struct timeval &ts)
+{
+    if (!_done_timestamp.tv_sec) {
+	_done_timestamp = ts;
+	return;
+    }
+
+    // determine left boundary for dead flows
+    struct timeval kill_dead_since = _done_timestamp;
+    kill_dead_since.tv_sec -= 120; // 2 minutes
+
+    // pass over flows
+    Flow *free_list = _done_head;
+    Flow **prev_ptr = &free_list;
+
+    Flow *flow = free_list;
+    while (flow) {
+	Flow *next = flow->free_next();
+	if (!flow->session_over()) {
+	    // reuse of a port; take it off the free-tracked list
+	    *prev_ptr = next;
+	    flow->clear_free_tracked();
+	} else if (flow->used_since(kill_dead_since))
+	    break;
+	else
+	    prev_ptr = &flow->_free_next;
+	flow = next;
+    }
+
+    // cut off free_list before 'flow'
+    *prev_ptr = 0;
+
+    // move free_head forward, to 'flow' or beyond
+    if (flow && flow->free_next()) {
+	// if 'flow' exists, then shift it to the end of the list
+	_done_head = flow->free_next();
+	flow->_free_next = 0;
+	flow->append_to_free(_done_head, _done_tail);
+    } else
+	_done_head = _done_tail = flow;
+
+    // free contents of free_list
+    while (free_list) {
+	assert(free_list->is_primary());
+	write_flow(free_list);
+	free_list = free_list->free_from_free(_map);
+    }
+
+    if (_done_head)
+	_done_timestamp = _done_head->last_session_timestamp();
+    _done_timestamp.tv_sec += 120;
+}
+
+Packet *
+CollectTCPFlows::handle_packet(Packet *p)
+{
+    const click_ip *iph = p->ip_header();
+    if (!iph)
+	return bad_packet(p);
+
+    IPFlowID flowid(p);
+    Flow *flow = _map[flowid];
+
+    if (!flow)
+	flow = add_flow(flowid, p);
+    if (flow) {
+	flow->update(p);
+	if (flow->session_over() && !flow->free_tracked())
+	    flow->add_to_free_tracked_tail(_done_head, _done_tail);
+    }
+
+    if (timercmp(&p->timestamp_anno(), &_done_timestamp, >))
+	pass_over_done(p->timestamp_anno());
+    
+    return p;
+}
+
+void
+CollectTCPFlows::push(int, Packet *p)
+{
+    if (Packet *q = handle_packet(p))
+	output(0).push(q);
+}
+
+Packet *
+CollectTCPFlows::pull(int)
+{
+    Packet *p = input(0).pull();
+    if (p)
+	p = handle_packet(p);
+    return p;
+}
+
+void
+CollectTCPFlows::write_flow(const Flow *flow)
+{
+    StringAccum sa;
+    sa << flow->first_session_timestamp() << ' '
+       << flow->last_session_timestamp() << ' '
+       << flow->_flow << ' ' << flow->_packet_count << ' '
+       << flow->_byte_count << '\n';
+    fwrite(sa.data(), 1, sa.length(), _f);
+}
+
+int
+CollectTCPFlows::clear_handler(const String &, Element *e, void *, ErrorHandler *)
+{
+    CollectTCPFlows *ctf = static_cast<CollectTCPFlows *>(e);
+    ctf->clear(true);
+    return 0;
+}
+
+void
+CollectTCPFlows::add_handlers()
+{
+    add_write_handler("clear", clear_handler, 0);
+}
+
+ELEMENT_REQUIRES(userlevel)
+EXPORT_ELEMENT(CollectTCPFlows)
+
+#include <click/bighashmap.cc>
---
+++ models/package/collectflows.hh
@@ -0,0 +1,171 @@
+// -*- mode: c++; c-basic-offset: 4 -*-
+#ifndef CLICK_COLLECTTCPFLOWS_HH
+#define CLICK_COLLECTTCPFLOWS_HH
+#include <click/element.hh>
+#include <click/ipflowid.hh>
+#include <click/bighashmap.hh>
+
+class CollectTCPFlows : public Element { public:
+
+    class Flow;
+
+    CollectTCPFlows();
+    ~CollectTCPFlows();
+
+    const char *class_name() const	{ return "CollectTCPFlows"; }
+    CollectTCPFlows *clone() const	{ return new CollectTCPFlows; }
+
+    void notify_noutputs(int);
+    int configure(const Vector<String> &, ErrorHandler *);
+    int initialize(ErrorHandler *);
+    void uninitialize();
+    void add_handlers();
+
+    Packet *handle_packet(Packet *);
+    void push(int, Packet *);
+    Packet *pull(int);
+
+    void write_flow(const Flow *);
+    
+  private:
+
+    typedef BigHashMap<IPFlowID, Flow *> Map;
+    Map _map;
+    Flow *_last_flow;
+
+    Flow *_done_head;
+    Flow *_done_tail;
+    struct timeval _done_timestamp;
+
+    FILE *_f;
+    String _filename;
+    
+    Packet *bad_packet(Packet *);
+    Flow *add_flow(const IPFlowID &, Packet *);
+    void pass_over_done(const struct timeval &);
+
+    void clear(bool write);
+
+    static int clear_handler(const String &, Element*, void*, ErrorHandler*);
+    
+};
+
+
+class CollectTCPFlows::Flow { public:
+
+    static bool make_pair(const IPFlowID &, Flow **, Flow **);
+
+    const IPFlowID &flow_id() const	{ return _flow; }
+    
+    const Flow *primary() const	{ return _is_primary ? this : _reverse; }
+    Flow *primary()		{ return _is_primary ? this : _reverse; }
+    bool is_primary() const	{ return _is_primary; }
+    Flow *reverse() const	{ return _reverse; }
+
+    const struct timeval &first_session_timestamp() const;
+    const struct timeval &last_session_timestamp() const;
+
+    void clear(bool is_primary);
+    
+    bool session_over() const	{ return _flow_over && _reverse->_flow_over; }
+    void set_flow_over()	{ _flow_over = true; }
+    void set_session_over()	{ _flow_over = _reverse->_flow_over = true; }
+
+    bool free_tracked() const	{ return _free_tracked; }
+    Flow *free_next() const	{ return _free_next; }
+    inline void add_to_free_tracked_tail(Flow *&head, Flow *&tail);
+    inline void append_to_free(Flow *&head, Flow *&tail);
+    inline Flow *free_from_free(Map &);
+    void clear_free_tracked()	{ _free_tracked = 0; }
+
+    inline bool used_since(const struct timeval &) const;
+    
+    inline void update(const Packet *, CollectTCPFlows *);
+    
+  private:
+    
+    IPFlowID _flow;
+    Flow *_reverse;
+    
+    uint32_t _packet_count;
+    uint64_t _byte_count;
+    
+    struct timeval _first_ts;
+    struct timeval _last_ts;
+    
+    bool _is_primary : 1;
+    bool _flow_over : 1;
+    bool _free_tracked : 1;
+    
+    Flow *_free_next;
+
+    Flow(const IPFlowID &, bool);
+
+    friend class CollectTCPFlows;
+    
+};
+
+inline
+CollectTCPFlows::Flow::Flow(const IPFlowID &flowid, bool is_primary)
+    : _flow(flowid), _packet_count(0), _byte_count(0),
+      _is_primary(is_primary), _flow_over(false), _free_tracked(false),
+      _free_next(0)
+{
+    timerclear(&_first_ts);
+    timerclear(&_last_ts);
+}
+
+inline void
+CollectTCPFlows::Flow::clear(bool is_primary)
+{
+    _packet_count = 0;
+    _byte_count = 0;
+    _is_primary = is_primary;
+    _flow_over = false;
+    timerclear(&_first_ts);
+    timerclear(&_last_ts);
+}
+
+inline void
+CollectTCPFlows::Flow::append_to_free(Flow *&head, Flow *&tail)
+{
+    assert((!head && !tail)
+	   || (head && tail && head->_free_tracked && tail->_free_tracked));
+    assert(!_free_next && is_primary());
+    if (tail)
+	tail = tail->_free_next = this;
+    else
+	head = tail = this;
+}
+
+inline void
+CollectTCPFlows::Flow::add_to_free_tracked_tail(Flow *&head, Flow *&tail)
+{
+    assert(!_free_tracked);
+    _free_tracked = _reverse->_free_tracked = true;
+    primary()->append_to_free(head, tail);
+}
+
+inline bool
+CollectTCPFlows::Flow::used_since(const struct timeval &when) const
+{
+    return timercmp(&_last_ts, &when, >) || timercmp(&_reverse->_last_ts, &when, >);
+}
+
+inline const struct timeval &
+CollectTCPFlows::Flow::first_session_timestamp() const
+{
+    // by definition, primary's first timestamp is first
+    return primary()->_first_ts;
+}
+
+inline const struct timeval &
+CollectTCPFlows::Flow::last_session_timestamp() const
+{
+    if (timercmp(&_last_ts, &_reverse->_last_ts, >))
+	return _last_ts;
+    else
+	return _reverse->_last_ts;
+}
+
+#endif
