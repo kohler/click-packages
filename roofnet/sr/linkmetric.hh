fc0a198e6425709d174e08e533af84039bd937b8
eddietwo <eddietwo>
2006-05-20 02:51:45 +0000

    add version of roofnet elements, taken from right before they were deleted from the repository

---
+++ roofnet/sr/linkmetric.hh
@@ -0,0 +1,19 @@
+#ifndef LINKMETRIC_HH
+#define LINKMETRIC_HH
+#include <click/element.hh>
+#include <click/ipaddress.hh>
+#include "ettstat.hh"
+CLICK_DECLS
+
+
+class LinkMetric : public Element {
+public:
+  LinkMetric() { }
+  
+  virtual ~LinkMetric() { }
+
+
+};
+
+CLICK_ENDDECLS
+#endif
